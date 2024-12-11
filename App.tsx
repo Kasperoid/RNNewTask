58322cea4ef9f58e46c004890e150353df19086e
Kasperoid <ivanu9053@gmail.com>
2024-12-11 11:03:14 +0300

    Подключение react-native-sheet-transition к проекту

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import {Provider} from 'react-redux';
 import store from './redux/store';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {RoutesContainer} from './routes/RoutesContainer';
+import {SheetProvider} from 'react-native-sheet-transitions';
 
 function App() {
   return (
-    <GestureHandlerRootView>
-      <Provider store={store}>
-        <RoutesContainer />
-      </Provider>
-    </GestureHandlerRootView>
+    <SheetProvider>
+      <GestureHandlerRootView>
+        <Provider store={store}>
+          <RoutesContainer />
+        </Provider>
+      </GestureHandlerRootView>
+    </SheetProvider>
   );
 }
 
